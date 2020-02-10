0c74a08702cf43d75ee5ad913d4cca5b1170ba8d
zifanwei <48812337+zifanwei@users.noreply.github.com>
2020-02-10 00:55:09 +0000

    Add files via upload

---
+++ Assets/Fish simulation/script/Flock.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public float speed = 0.1f;
+    // decides how fast the fish will turn
+    float rotationSpeed = 4.0f;
+    Vector3 averageHeading;
+    Vector3 averagePosition;
+    float neighbourDistance = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(0.5f, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 5) < 1)
+        {
+            ApplyRules();
+        }
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = GlobalFlock.allFish;
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.1f;
+        Vector3 goalPosition = GlobalFlock.goalPosition;
+        float distance;
+        int groupSize = 0;
+        foreach (GameObject gmo in gos)
+        {
+            //every fish needs to know the position of other fishes and their distances
+            if(gmo != this.gameObject)
+            {
+                distance = Vector3.Distance(gmo.transform.position, this.transform.position);
+                if(distance<= neighbourDistance)
+                {
+                    vcentre += gmo.transform.position;
+                    groupSize++;
+
+                    //avoid of being in a small distance
+                    if(distance < 0.1f)
+                    {
+                        vavoid = vavoid + (this.transform.position - gmo.transform.position);
+                    }
+                    Flock anotherFlock = gmo.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+        }
+        if(groupSize > 0)
+        {
+            vcentre = vcentre / groupSize + (goalPosition - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if(direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                      Quaternion.LookRotation(direction),
+                                                      rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Fish simulation/script/GlobalFlock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject fishPrefab;
+    static int numOfFish = 10;
+    public static GameObject[] allFish = new GameObject[numOfFish];
+    public static int tankSize = 5;
+    public static Vector3 goalPosition = Vector3.zero;
+    void Start()
+    {
+        for(int i = 0; i< numOfFish; i++)
+        {
+            // create a position of fish
+            Vector3 position = new Vector3(Random.Range(-tankSize, tankSize),
+                                           Random.Range(-tankSize, tankSize),
+                                           Random.Range(-tankSize, tankSize));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //reset the movement randomly
+        if (Random.Range(0, 10000) < 50)
+        {
+            goalPosition = new Vector3(Random.Range(-tankSize, tankSize),
+                                           Random.Range(-tankSize, tankSize),
+                                           Random.Range(-tankSize, tankSize));
+        }
+    }
+}
